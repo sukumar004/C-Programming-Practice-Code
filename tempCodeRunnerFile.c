20412aef13fae5093f484544e7494a7544eb1992
sukumar004 <dpsukumardeve2303@gmail.com>
2025-04-18 23:33:16 +0530

    Initial Commit

--- tempCodeRunnerFile.c
+++
@@ -1,9 +0,0 @@
-    // strupr(word);  // Convert to UPPER CASE
-    // strlwr(word); // Convert to Lower Case
-    // strcat(word,concatenate); // Appende Hello to end of input from word, output like "work Hello"
-    // strncat(word,concatenate,2); // Append two characters from concatenate and added to end of word output like "work He"
-    // strcpy(copied,word);  // Here we copy the word to copied variable
-    // strncpy(copied,word,5); // Here we only copy how many characters we want to copy,,?
-    // strset(concatenate,'X'); // Here we set the character to a variable
-    // strnset(concatenate,'S',1); //Here we set the char with how much we want to set was defined
-    // strrev(concatenate); // Here we reverse the string characters
